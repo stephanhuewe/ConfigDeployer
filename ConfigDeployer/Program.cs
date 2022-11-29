1ad2dd822d61017ccb4744a497f85548d66841e9
stephanhuewe <sh@huewe.info>
2022-11-29 22:48:25 +0100

    Restructuring Code, First tests, Adding Demo file

--- ConfigDeployer/Program.cs
+++ ConfigDeployer/Program.cs
@@ -5,29 +5,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        var configFileFromPath = new ConfigParser("config.cnf");
-        string configFile = configFileFromPath.GetValue("Config", "configfile");    
-        string hostOrIP = configFileFromPath.GetValue("Server", "hostOrIP");
-        string port = configFileFromPath.GetValue("Server", "port");
-        string username = configFileFromPath.GetValue("Server", "username");
-        string password = configFileFromPath.GetValue("Server", "password");
-             
+        string file = "config.cnf";
+        if (args.Length == 1)
+        {
+            file = args[0];
+        }             
 
-        // Setup Credentials and Server Information
-        ConnectionInfo ConnNfo = new ConnectionInfo(hostOrIP, Int32.Parse(port), username,
-            new AuthenticationMethod[]{
+        var configFileFromPath = new ConfigParser(file);
 
-                // Pasword based Authentication
-                new PasswordAuthenticationMethod(username,password)                
+        ConnectionInfo connectionInfo = SetupConnection(configFileFromPath);
+        DeployFile(configFileFromPath, connectionInfo);
+        RestartService(configFileFromPath, connectionInfo);
+        Console.ReadKey();
+    }
+
+    private static void RestartService(ConfigParser configFileFromPath, ConnectionInfo connectionInfo)
+    {
+        string servicename = configFileFromPath.GetValue("Service", "servicename");
+        bool restart = bool.Parse(configFileFromPath.GetValue("Service", "restart"));
+
+        if (restart)
+        {
+            using (var sshclient = new SshClient(connectionInfo))
+            {
+                sshclient.Connect();
+
+                Console.WriteLine(sshclient.CreateCommand($"/etc/init.d/{servicename} restart").Execute());
+                sshclient.Disconnect();
             }
-        );
+        }
+    }
 
+    private static void DeployFile(ConfigParser configFileFromPath, ConnectionInfo connectionInfo)
+    {
         string SourceConfigFile = configFileFromPath.GetValue("Config", "SourceConfigFile");
         string TargetConfigFile = configFileFromPath.GetValue("Config", "TargetConfigFile");
         string TargetConfigPath = configFileFromPath.GetValue("Config", "TargetConfigPath");
 
         // deploy config file
-        using (var sftp = new SftpClient(ConnNfo))
+        using (var sftp = new SftpClient(connectionInfo))
         {
             string uploadfn = SourceConfigFile;
 
@@ -40,21 +56,24 @@ class Program
             sftp.Disconnect();
         }
 
-        string servicename = configFileFromPath.GetValue("Config", "servicename");
-        bool restart = bool.Parse(configFileFromPath.GetValue("Config", "restart"));
+        Console.WriteLine($"Uploading file {SourceConfigFile} to {TargetConfigPath}/{TargetConfigFile}");
+    }
 
-        if (restart)
-        {
+    private static ConnectionInfo SetupConnection(ConfigParser configFileFromPath)
+    {
+        string hostOrIP = configFileFromPath.GetValue("Server", "hostOrIP");
+        string port = configFileFromPath.GetValue("Server", "port");
+        string username = configFileFromPath.GetValue("Server", "username");
+        string password = configFileFromPath.GetValue("Server", "password");
+      
+        ConnectionInfo ConnNfo = new ConnectionInfo(hostOrIP, Int32.Parse(port), username,
+            new AuthenticationMethod[]{
 
-            // Execute (SHELL) Commands
-            using (var sshclient = new SshClient(ConnNfo))
-            {
-                sshclient.Connect();
-
-                Console.WriteLine(sshclient.CreateCommand($"/etc/init.d/{servicename} restart").Execute());
-                sshclient.Disconnect();
+                // Pasword based Authentication
+                new PasswordAuthenticationMethod(username,password)
             }
-        }
-        Console.ReadKey();
+        );
+        Console.WriteLine($"Getting Connection to {hostOrIP} - Port {port} with user {username}");
+        return ConnNfo;
     }
 }
